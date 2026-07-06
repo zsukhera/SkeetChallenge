80b498b8fb023039aa6314d3ba336445af35d7b3
zsukhera <zain2466sukhera@gmail.com>
2026-07-06 13:00:13 +0500

    addition of Muzzleflash

--- Assets/scripts/gun.cs
+++ Assets/scripts/gun.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 using StarterAssets;
+using System.Collections;
 
 public class gun : MonoBehaviour
 {
+    public GameObject audioManager;
     private StarterAssetsInputs input;
     private bool wasFirePressed;
     private  recoil recoilscript;
+    [Header ("Useable Objects")]
+    public GameObject muzzleflash;
     void Start()
     {
         //input = FindObjectOfType<StarterAssetsInputs>();
         recoilscript = GetComponent<recoil>();
         Debug.Log(FindObjectsOfType<StarterAssetsInputs>().Length);
+        //muzzleflash = GameObject.Find("muzzleFlash");
+        //since muzzle flash is initially set as false
+        //we cannot use FindWithTag()
+        if (!muzzleflash)
+            Debug.Log("Did not find muzzleflash");
+        muzzleflash.SetActive(false);
     }
 
     void HandleFireInput()
@@ -18,7 +28,19 @@ public class gun : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Fire();
+            StartCoroutine(muzzleFlash());
         }
+    }
+
+
+    IEnumerator muzzleFlash()
+    {
+        Debug.Log("Muzzle flash on");
+        muzzleflash.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        muzzleflash.SetActive(false);
+        Debug.Log("Muzzle flash off");
+
     }
 
     void Update()
